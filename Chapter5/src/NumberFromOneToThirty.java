8877bf945053fc7bd2e4b4ad2c71783c406908b2
Bigmehelen <getfitbyhelena@gmail.com>
2025-07-14 05:35:09 +0100

    todays task

---
+++ Chapter5/src/NumberFromOneToThirty.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NumberFromOneToThirty {
+    public static void main(String[] args) {
+
+        for (int count = 1; count <= 30; count++) {
+                if (count % 3 == 0) {
+                    System.out.println(count);
+                }
+            }
+        }
+    }
